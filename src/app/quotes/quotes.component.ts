f05a3abe353f145e2e08686f689ae0fe7b8ac323
Your Name <you@example.com>
2018-05-28 06:20:13 +0300

    Added under the component class quotes the quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -27,6 +27,11 @@ export class QuotesComponent implements OnInit {
         }
         }
       }
+    VoteUp(isComplete,index){
+      if (isComplete){
+       this.votes
+      }
+    } 
         
   Details(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
