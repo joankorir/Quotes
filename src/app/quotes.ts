6d0898650195b7c8a3fa576c3dc2fe47531718c9
Your Name <you@example.com>
2018-05-28 06:41:29 +0300

    Added an upvote button

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,8 +2,8 @@ import { Timeouts } from "../../Quotes/node_modules/@types/selenium-webdriver";
 
 export class Quotes {
     public  showDescription:Boolean;
-    
-    constructor(public id:Number, public name:string, public Person:string,public Votes:Number){
+
+    constructor(public id:Number, public name:string, public Person:string,public Votes:Number,public DurationTime:Date){
         this.showDescription=false
     }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,27 +8,27 @@ import { Component, OnInit } from '@angular/core'
 })
 export class QuotesComponent implements OnInit {
   quotes=[
-    new Quotes(1,'Be in love with your life.Every minute','Jack Kerouac',34),
-    new Quotes(2,'Know Your Worth','Hart Kevin',45),
-    new Quotes (3, 'Your life is your Magic Wand','Dalai Lama',23),
-    new Quotes(4,'My Bestfriend is the one who brings the best in me','Steve Martins',89),
-    new Quotes (5,'No Frienship is an Accident','Milton Beele',98),
-    new Quotes(6,'I almost give a fuck scared the shit out of myself','George Burns',19),
-    new Quotes(7,'I am not lazy i am on Energy saving Mode','Thomas  Sowel',12),
-     
+    new Quotes(1,'Be in love with your life.Every minute','Jack Kerouac',34new Date(1999,3,20)),
+    new Quotes(2,'Know Your Worth','Hart Kevin',4new Date(1999,4,12)),
+    new Quotes (3, 'Your life is your Magic Wand','Dalai Lama',23new Date(1999,7,12)),
+    new Quotes(4,'My Bestfriend is the one who brings the best in me','Steve Martins',89new Date(1999,8,19)),
+    new Quotes (5,'No Frienship is an Accident','Milton Beele',98new Date(1999,1,11)),
+    new Quotes(6,'I almost give a fuck scared the shit out of myself','George Burns',19new Date(1999,3,10)),
+    new Quotes(7,'I am not lazy i am on Energy saving Mode','Thomas  Sowel',12new Date(1999,6,18)),
+
   ]
 
   DeleteQuotes(isComplete,index){
     if (isComplete){
       let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].name}`)
-      
+
     if (isComplete){
         this.quotes.splice(index,1);
         }
         }
       }
-   
-        
+
+
   Details(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
@@ -38,7 +38,7 @@ export class QuotesComponent implements OnInit {
 
 
   constructor() { }
-    
+
 
   ngOnInit() { }
 }
